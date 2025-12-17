7ca69eb827482e0ef2b97cb9e950e517a60122ba
Swaggiste <156951472+Swaggiste@users.noreply.github.com>
2025-12-17 12:05:58 +0300

    redeploy

--- sendmail.php
+++ sendmail.php
@@ -1,20 +1,28 @@
 <?php
-if(isset($_POST['submit'])){
-    $to = "blasthits@gmail.com"; // Your cPanel email
-    $subject = "New message from your website";
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+// Allow CORS just in case (optional)
+header("Access-Control-Allow-Origin: *");
 
-    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+// Collect form data safely
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$message = isset($_POST['message']) ? $_POST['message'] : '';
 
-    $headers = "From: info@xflirta.com\r\n";
-    $headers .= "Reply-To: $email\r\n";
+// Recipient email
+$to = "blasthits@gmail.com";
+$subject = "New message from your website";
 
-    if(mail($to, $subject, $body, $headers)){
-        echo "success";
-    } else {
-        echo "failed";
-    }
+// Email body
+$body = "Name: $name\nEmail: $email\nMessage:\n$message";
+
+// Headers
+$headers = "From: Zablon Consulting <info@xflirta.com>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
+
+// Send the email
+if(mail($to, $subject, $body, $headers)){
+    echo "success";
+} else {
+    echo "failed";
 }
 ?>
